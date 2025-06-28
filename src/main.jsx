b21f5a2c7047f10d4deb62c335db58be89937b76
Bhavin Prajapati <prajapatibhavin989@gmail.com>
2025-06-28 22:59:58 +0530

    Refactor routing structure in main.jsx for improved readability

--- src/main.jsx
+++ src/main.jsx
@@ -16,65 +16,65 @@ import Post from "./pages/Post";
 import AllPost from "./pages/AllPost";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-        {
-            path: "/",
-            element: <Home />,
-        },
-        {
-            path: "/login",
-            element: (
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/login",
+                element: (
                     <Login />
-            ),
-        },
-        {
-            path: "/signup",
-            element: (
+                ),
+            },
+            {
+                path: "/signup",
+                element: (
                     <Signup />
-            ),
-        },
-        {
-            path: "/all-posts",
-            element: (
-                <AuthLayout authentication>
-                    {" "}
-                    <AllPost />
-                </AuthLayout>
-            ),
-        },
-        {
-            path: "/add-post",
-            element: (
-                <AuthLayout authentication>
-                    {" "}
-                    <AddPost />
-                </AuthLayout>
-            ),
-        },
-        {
-            path: "/edit-post/:slug",
-            element: (
-                <AuthLayout authentication>
-                    {" "}
-                    <EditPost />
-                </AuthLayout>
-            ),
-        },
-        {
-            path: "/post/:slug",
-            element: <Post />,
-        },
-    ],
-},
+                ),
+            },
+            {
+                path: "/all-posts",
+                element: (
+                    <AuthLayout authentication>
+                        {" "}
+                        <AllPost />
+                    </AuthLayout>
+                ),
+            },
+            {
+                path: "/add-post",
+                element: (
+                    <AuthLayout authentication>
+                        {" "}
+                        <AddPost />
+                    </AuthLayout>
+                ),
+            },
+            {
+                path: "/edit-post/:slug",
+                element: (
+                    <AuthLayout authentication>
+                        {" "}
+                        <EditPost />
+                    </AuthLayout>
+                ),
+            },
+            {
+                path: "/post/:slug",
+                element: <Post />,
+            },
+        ],
+    },
 ])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router}/>
-    </Provider>
-  </StrictMode>,
+    <StrictMode>
+        <Provider store={store}>
+            <RouterProvider router={router} />
+        </Provider>
+    </StrictMode>,
 )
