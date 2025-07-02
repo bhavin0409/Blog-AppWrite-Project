920b903b28cd6b617b9dfbdccb23f250451106d9
Bhavin Prajapati <prajapatibhavin989@gmail.com>
2025-07-02 22:25:06 +0530

    Add _redirects file

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,88 +1,92 @@
 import React, { useState } from 'react'
 import { data, Link, useNavigate } from 'react-router-dom'
 import authService from '../appwrite/auth.service'
-import { Button , Input  } from './index'
+import { Button, Input } from './index'
 import { useDispatch } from 'react-redux'
 import { login as authLogin } from '../store/authSlice'
-import { useForm } from 'react-hook-form' 
+import { useForm } from 'react-hook-form'
 import Logo from './Logo/Logo'
 
 const Login = () => {
     const navigate = useNavigate()
     const dispatch = useDispatch()
-    const { register , handleSubmit } = useForm()
+    const { register, handleSubmit } = useForm()
     const [error, setError] = useState('')
 
     const login = async (data) => {
-        setError('')
+        setError('');
         try {
-            const session = await authService.login(data)
-            if (session) {
-                const userData = await authService.getCurrentUser()
-                if (userData) {
-                    dispatch(authLogin(userData))
-                }
-            navigate('/')
-            }
+            const session = await authService.login(data);
+            console.log(session);
             
+            if (session) {
+                const userData = await authService.getCurrentUser();
+                if (userData) {
+                    dispatch(authLogin({ userData }));
+                }
+                navigate('/');
+            }else{
+                setError('Login failed. Please try again.');
+            }
         } catch (error) {
-            setError(error.message || 'Something went wrong')
+            console.error("Login error:", error);
+            setError(error.param);
         }
-    }
+    };
 
-  return (
-    <div className='flex items-center justify-center w-full'>
-        <div className='mx-auto max-w-lg w-full p-10 bg-gray-100 rounded-xl shadow-md border-black/10'>
-            <div className='mb-2 flex justify-center '>
-                <span className='inline-block w-full max-w-[100px]'>
-                    <Logo width='100%' />
-                </span>
+    return (
+        <div className='flex items-center justify-center w-full'>
+            <div className='mx-auto max-w-lg w-full p-10 bg-gray-100 rounded-xl shadow-md border-black/10'>
+                <div className='mb-2 flex justify-center '>
+                    <span className='inline-block w-full max-w-[100px]'>
+                        <Logo width='100%' />
+                    </span>
+                </div>
+                <h2 className='text-center text-2xl font-bold leading-tight'>sign in to your account</h2>
+
+                <form onSubmit={handleSubmit(login)} className='mt-8 space-y-6'>
+                    <Input
+                        type='email'
+                        label='Email :'
+                        placeholder='Enter your email'
+                        {...register('email',
+                            {
+                                required: true,
+                                validate: {
+                                    matchPattern: (value) => /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{1,3}$/.test(value) || 'Please enter a valid email address'
+                                }
+                            })}
+                    />
+                    <Input
+                        type='password'
+                        label='Password :'
+                        placeholder='Enter your password'
+                        {...register('password',
+                            {
+                                required: true,
+                                minLength: {
+                                    value: 6,
+                                    message: 'Password must be at least 6 characters long'
+                                }
+                            })}
+                    />
+
+                    <Button
+                        type='submit'
+                        classname='w-full'
+                    > Sign in</Button>
+                </form>
+
+                <p className='mt-2 text-center text-base text-black/60'>
+                    Don&apos;t have an account?
+                    <Link to='/signup' className='font-medium duration-200 transition-all text-blue-500 hover:underline'>Sign Up</Link>
+                </p>
+                {error && (
+                    <p className='text-red-500 text-center mt-8'>{error}</p>
+                )}
             </div>
-            <h2 className='text-center text-2xl font-bold leading-tight'>sign in to your account</h2>
-
-            <form onSubmit={handleSubmit(login)} className='mt-8 space-y-6'>
-                <Input
-                    type='email'
-                    label='Email :'
-                    placeholder='Enter your email'
-                    {...register('email' , 
-                        {
-                        required: true,
-                        validate: {
-                            matchPattern: (value) => /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{1,3}$/.test(value) || 'Please enter a valid email address'
-                        }
-                    })}
-                />
-                <Input
-                    type='password'
-                    label='Password :'
-                    placeholder='Enter your password'
-                    {...register('password' , 
-                        {
-                        required: true,
-                        minLength: {
-                            value: 6,
-                            message: 'Password must be at least 6 characters long'
-                        }
-                    })}
-                />
-
-                <Button
-                    type='submit'
-                    classname='w-full'
-                > Sign in</Button>
-            </form>
-
-            <p className='mt-2 text-center text-base text-black/60'>
-                Don&apos;t have an account?
-                <Link to='/signup' className='font-medium duration-200 transition-all text-blue-500 hover:underline'>Sign Up</Link>
-            </p>
-            { error && (
-                <p className='text-red-500 text-center mt-8'>{error}</p>
-            )}
         </div>
-    </div>
-  )
+    )
 }
 
 export default Login
--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -11,9 +11,9 @@ const RTE = ({
     defaultValue = '',
 }) => {
     return (
-        <div className='w-full'>
+        <div className="w-full">
             {label && (
-                <label className='inline-block mb-1 pl-1'>
+                <label className="inline-block mb-1 pl-1">
                     {label}
                 </label>
             )}
@@ -21,44 +21,50 @@ const RTE = ({
                 name={name || "content"}
                 control={control}
                 render={({ field: { onChange } }) => (
-                    <Editor
-                        apiKey={config.TinyMCE}
-                        initialValue={defaultValue}
-                        init={{
-                            initialValue: defaultValue,
-                            hieght: 500,
-                            menubar: true,
-                            plugins: [
-                                "image",
-                                "advlist",
-                                "autolink",
-                                "lists",
-                                "link",
-                                "image",
-                                "charmap",
-                                "preview",
-                                "anchor",
-                                "searchreplace",
-                                "visualblocks",
-                                "code",
-                                "fullscreen",
-                                "insertdatetime",
-                                "media",
-                                "table",
-                                "code",
-                                "help",
-                                "wordcount",
-                                "anchor",
-                            ],
-                            toolbar: "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help | code",
-                            content_style:  "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
-                        }}
-                        onEditorChange={onChange}
-                    />
+                    <div className="w-full">
+                        <Editor
+                            apiKey={config.TinyMCE}
+                            initialValue={defaultValue}
+                            init={{
+                                initialValue: defaultValue,
+                                height: 500,
+                                width: '100%', // Make editor take full width of its container
+                                menubar: true,
+                                plugins: [
+                                    "image",
+                                    "advlist",
+                                    "autolink",
+                                    "lists",
+                                    "link",
+                                    "image",
+                                    "charmap",
+                                    "preview",
+                                    "anchor",
+                                    "searchreplace",
+                                    "visualblocks",
+                                    "code",
+                                    "fullscreen",
+                                    "insertdatetime",
+                                    "media",
+                                    "table",
+                                    "code",
+                                    "help",
+                                    "wordcount",
+                                    "anchor",
+                                ],
+                                toolbar:
+                                    "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help | code",
+                                content_style:
+                                    "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+                                // No setup needed for resizeTo
+                            }}
+                            onEditorChange={onChange}
+                        />
+                    </div>
                 )}
             />
         </div>
-    ) || (<Loading/>)
+    ) || <Loading />;
 }
 
 export default RTE
